e3798a5dacfca30960a6b06fef68a86c4fee0920
Laura <laura.e.vollmer@gmail.com>
2025-09-30 13:24:39 -0500

    Creating first model

---
+++ server/models/InventoryItem.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+//Schema
+const InventoryItemSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+        trim: true,
+    },
+    quantitiy: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    price: {
+        type: Number,
+        required: false,
+        min: 0,
+    },
+    category: {
+        type: String,
+        default: 'Uncategorized',
+    },
+    dateAdded: {
+        type: Date,
+        default: Date.now,
+    }
+})
