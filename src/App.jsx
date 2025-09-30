e9d7be337af021db191a1745d877a4b7cbe4a4ea
Laura <laura.e.vollmer@gmail.com>
2025-09-30 11:04:10 -0500

    Created Database and FAQ components with react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
+// wrap entire app in routing
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-     <h1>Hello</h1>
+      <BrowserRouter>
+        <h1>Hello</h1>
+        <Routes>
+          <Route path="/database" element={<Database />} />
+          <Route path="FAQ" element={<FAQ/>} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
