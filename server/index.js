73c8d52be8c6f7b1db9a7fd6930ebf2458bdb3bc
Laura <laura.e.vollmer@gmail.com>
2025-09-30 13:32:13 -0500

    Updating index.js

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 // load environment variables
 require('dotenv').config();
 
-
 // server setup express
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require ('cors');
 
+// setting up express and PORT
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -14,10 +14,14 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Routes
+const inventoryRoutes = require('./routes/inventory')
+app.use('/inventory', inventoryRoutes);
+
 //Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
 .then (() => console.log('MongoDB Atlas connected!'))
-.catch(err => console.error(err));
+.catch(err => console.error('Mongoose Error:', err));
 
 // GET endpoint
 app.get('/', (req, res) => {
