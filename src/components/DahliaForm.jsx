03416797a68666fcc2217d6da1f004c63aeea1f6
Laura <laura.e.vollmer@gmail.com>
2025-09-30 11:30:56 -0500

    Creating Form input

--- src/components/DahliaForm.jsx
+++ src/components/DahliaForm.jsx
@@ -1,9 +1,43 @@
-import {useState} from 'react'
+import { useState } from 'react'
 
-function DahliaForm () {
+function DahliaForm() {
 
   return (
     <>
+      <form>
+        <label>
+          Dahlia Variety Name:
+          <input></input>
+        </label>
+        <label>
+          Color:
+          <input></input>
+        </label>
+        <label>
+          Container ID:
+          <input></input>
+        </label>
+        <label>
+          Storage Location:
+          <input></input>
+        </label>
+        <label>
+          Purchase Source:
+          <input></input>
+        </label>
+        <label>
+          Purchase Year:
+          <input></input>
+        </label>
+        <label>
+          Number of Tubers:
+          <input></input>
+        </label>
+        <label>
+          Condition:
+          <input></input>
+        </label>
+      </form>
     </>
   )
 }
