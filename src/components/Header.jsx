faebdc24c7d4fa22a3d5d7bdd47a5405fdd9a621
Laura <laura.e.vollmer@gmail.com>
2025-10-01 15:01:37 -0500

    Adding hamburger for navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,15 @@ import Navigation from "./Navigation"
 import '../App.css'
 import { LuFlower2 } from "react-icons/lu";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { useState } from "react";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = () => {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <div className='header-bar'>
       <div className='logo'>
@@ -11,10 +18,10 @@ const Header = () => {
         Dahlia Petal Ledger
       </div>
       <div>
-        <RxHamburgerMenu />
-      </div>
-      <div>
-        <Navigation />
+        <button onClick={toggleOpen}>
+          <RxHamburgerMenu size="1.5em" />
+        </button>
+        {isOpen && <div className="faq-answer"><Navigation /></div>}
       </div>
     </div>
   )
