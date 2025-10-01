d32a526b74a3bbc0d98ae51ac1d581efe7910820
Laura <laura.e.vollmer@gmail.com>
2025-10-01 09:54:04 -0500

    Updating the Database with UI display of data

--- src/components/Database.jsx
+++ src/components/Database.jsx
@@ -1,11 +1,13 @@
 import Navigation from "./Navigation";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import InventoryItem from "../../server/models/InventoryItem";
 
 const Database = () => {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  useEffect (() => {
+  useEffect(() => {
     axios.get('http://localhost:5000/api/dahliainventorymanagement')
       .then(response => {
         setData(response.data);
@@ -15,13 +17,22 @@ const Database = () => {
       });
   }, []);
 
+  if (loading) return <p>Loading...</p>;
+
   return (
     <div>
       <div>
         <Navigation />
       </div>
       <div>
-
+        <h1>Dahlia Inventory Management</h1>
+        <ul>
+          {InventoryItem.map(InventoryItem => (
+            <li key={InventoryItem.id}>
+              {InventoryItem.name}
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   )
