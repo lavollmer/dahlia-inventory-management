59af0e5fee8cfe2cac32ddc90ab0c27cb2bf59f7
Laura <laura.e.vollmer@gmail.com>
2025-10-01 14:48:49 -0500

    Updating homepage with database view and help FAQ section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,6 +7,8 @@ import { useNavigate } from 'react-router-dom'
 import { TbCircleNumber1Filled } from "react-icons/tb";
 import { TbCircleNumber2Filled } from "react-icons/tb";
 import { TbCircleNumber3Filled } from "react-icons/tb";
+import { LuDatabase } from "react-icons/lu";
+import { FaQuestionCircle } from "react-icons/fa";
 
 const Home = () => {
     const navigate = useNavigate();
@@ -57,6 +59,20 @@ const Home = () => {
                             <h3>Click the eye icon to view details, the pencil icon to edit information, or the trash icon to remove a dahlia from your inventory. Keep your records accurate and up-to-date.</h3>
                         </div>
                     </div>
+                </div>
+                <div className='how-to-use-box'>
+                    <div className='title-use'>
+                        <LuDatabase size="2em"  />
+                        <h1> View Database</h1>
+                    </div>
+                    <p>Browse and manage your dahlia collection</p>
+                </div>
+                <div className='how-to-use-box'>
+                    <div className='title-use'>
+                        <FaQuestionCircle size="2em" />
+                        <h1>Help & FAQ</h1>
+                    </div>
+                    <p>Get answers to common questions and learn tips</p>
                 </div>
                 <div>
                     <Footer />
