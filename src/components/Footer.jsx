5dd53ea3e4cd6e175125fed2d284d6d3472df426
Laura <laura.e.vollmer@gmail.com>
2025-10-02 10:22:50 -0500

    Updating the navigation tool and dropdown

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 const Footer = () => {
   return (
-     <div className='footer'>
-      <p>Created by <span>Laura Dev</span> · 2025 &nbsp;|&nbsp; 
-      <button>
-        <a
-          href="https://github.com/lavollmer"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          View on GitHub
-        </a>
+    <div className='footer'>
+      <p>Created by Laura Dev · 2025 &nbsp;|&nbsp;
+        <button>
+          <a
+            href="https://github.com/lavollmer"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            View on GitHub
+          </a>
         </button>
       </p>
     </div>
