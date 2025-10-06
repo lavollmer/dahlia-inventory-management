c083d6bd19df7da1a5b1029d611d16f7382741ee
Laura <laura.e.vollmer@gmail.com>
2025-10-06 09:42:36 -0500

    Updating the individual tuber details page

--- src/components/Tuber.jsx
+++ src/components/Tuber.jsx
@@ -1,41 +1,41 @@
 import Header from './Header'
 import Footer from './Footer'
 import axios from 'axios';
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 const Tuber = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const fetchData = () => {
+  const fetchData = async () => {
     try {
-      const response = await axios.get("http://localhost:5000/inventory/:id");
+      const response = await axios.get(`http://localhost:5000/inventory/:{id}`);
       setData(response.data)
     } catch (err) {
       console.error("There was an error fetching data:", err)
     }
   }
 
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div>
       <Header />
-      <div>
+      <div className='individual-tuber'>
         <h1>Individual Tuber Details</h1>
         <div>
           {data.map(item => (
             <div key={item._id}>
               {item.name}
             </div>
-          )}
+          ))}
         </div>
       </div>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default Tuber
