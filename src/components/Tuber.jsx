2c80d9d36e202435d0ba172318beb2053c7bb5b8
Laura <laura.e.vollmer@gmail.com>
2025-10-06 09:55:06 -0500

    Debugging tuber jsx component page

--- src/components/Tuber.jsx
+++ src/components/Tuber.jsx
@@ -2,47 +2,60 @@ import Header from './Header'
 import Footer from './Footer'
 import axios from 'axios';
 import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
 
 const Tuber = () => {
-  const [data, setData] = useState([]);
+  // get the id from the url
+  const { id } = useParams();
+
+  // useState values
+  const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
 
   const fetchData = async () => {
     try {
-      const response = await axios.get(`http://localhost:5000/inventory/:{id}`);
+      const response = await axios.get(`http://localhost:5000/inventory/${id}`);
       setData(response.data)
     } catch (err) {
       console.error("There was an error fetching data:", err)
+    } finally {
+      setLoading(false);
     }
   }
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    if (id) fetchData();
+  }, [id]);
 
   return (
     <div>
       <Header />
       <div className='individual-tuber'>
         <h1>Individual Tuber Details</h1>
-        <div>
-          {data.map(item => (
-            <div key={item._id}>
-              <div> {item._id}</div>
-              <div> {item.name}</div>
-              <div> {item.variety} </div>
-              <div> {item.color} </div>
-              <div> {item.bloom_size} </div>
-              <div> {item.status} </div>
-              <div> {item.container_id} </div>
-              <div> {item.storage} </div>
-              <div> {item.purchase_source} </div>
-              <div> {item.purchase_year} </div>
-              <div> {item.number_of_tubers} </div>
-              <div> {item.condition} </div>
-            </div>
-          ))}
-        </div>
+        {loading ? (
+          <p>Loading ...</p>
+        ) : data ? (
+          <div>
+            {data.map(item => (
+              <div key={item._id}>
+                <div> {item._id}</div>
+                <div> {item.name}</div>
+                <div> {item.variety} </div>
+                <div> {item.color} </div>
+                <div> {item.bloom_size} </div>
+                <div> {item.status} </div>
+                <div> {item.container_id} </div>
+                <div> {item.storage} </div>
+                <div> {item.purchase_source} </div>
+                <div> {item.purchase_year} </div>
+                <div> {item.number_of_tubers} </div>
+                <div> {item.condition} </div>
+              </div>
+            ) : (
+              <p>No data found for this tuber.</p>
+          </div>
+           )}
       </div>
       <Footer />
     </div>
