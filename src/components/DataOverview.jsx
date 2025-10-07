5e0d782e59d5364b8baec6f186d836062a191aea
Laura <laura.e.vollmer@gmail.com>
2025-10-07 10:04:44 -0500

    Styling the analytics board

--- src/components/DataOverview.jsx
+++ src/components/DataOverview.jsx
@@ -2,6 +2,7 @@ import Header from "./Header"
 import Footer from "./Footer"
 import { useState, useEffect } from 'react'
 import axios from 'axios';
+import "../App.css"
 
 const DataOverview = () => {
   const [data, setData] = useState([]);
@@ -12,20 +13,22 @@ const DataOverview = () => {
       .catch(error => console.error("Error fetching data:", error))
   }, []);
 
+  const uniqueVarieties = new Set(data.map(item => item.variety));
+  const numberOfVarieties = uniqueVarieties.size;
+
   return (
     <div>
       <Header />
       <div className="analytics-page">
         <h1>Analytics</h1>
         <p>Analytics Dashboard</p>
-        <div>
-          {data.map(item => (
-            <p key={item._id}>{item.name}: {item.variety}</p>
-          ))}
+        <div className="summary">
+          <h2>Summary</h2>
+          <p>Total unique varieties: {numberOfVarieties}</p>
         </div>
       </div>
       <Footer />
-    </div>
+    </div >
   )
 }
 
