edf3eac37886500e7dcc3a776cbf219ca542339c
Laura <laura.e.vollmer@gmail.com>
2025-10-07 09:57:46 -0500

    Built analytics dashboard

--- src/components/DataOverview.jsx
+++ src/components/DataOverview.jsx
@@ -1,14 +1,27 @@
 import Header from "./Header"
 import Footer from "./Footer"
+import { useState, useEffect } from 'react'
+import axios from 'axios';
 
 const DataOverview = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/inventory')
+      .then(response => setData(response.data))
+      .catch(error => console.error("Error fetching data:", error))
+  }, []);
+
   return (
     <div>
       <Header />
-      <div>
+      <div className="analytics-page">
         <h1>Analytics</h1>
+        <p>Analytics Dashboard</p>
         <div>
-          <p>Analytics Dashboard</p>
+          {data.map(item => (
+            <p key={item._id}>{item.name}: {item.variety}</p>
+          ))}
         </div>
       </div>
       <Footer />
