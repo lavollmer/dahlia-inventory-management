65b25a7b2364edc9deadcdc1ad9062b0578b5926
Laura <laura.e.vollmer@gmail.com>
2025-10-08 11:52:20 -0500

    Updating and fixing the tubers by variety chart

--- src/graphs/TubersByVariety.jsx
+++ src/graphs/TubersByVariety.jsx
@@ -1,22 +1,28 @@
 import "../App.css"
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
-
+import {
+    BarChart,
+    Bar,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+} from 'recharts';
 
 const TubersByVariety = ({ inventory }) => {
-
+    if (!inventory || inventory.length === 0) return <p>No data available.</p>;
     return (
         <>
             <div>
+                <h1>Tubers By Variety</h1>
                 <div>
-                    <BarChart data={inventory}>
+                    <BarChart width={600} height={300} data={inventory}>
+                        <CartesianGrid strokeDasharray="3 3" />
                         <XAxis dataKey="name" />
                         <YAxis />
                         <Tooltip />
                         <Bar dataKey="number_of_tubers" fill="#82ca9d" />
                     </BarChart >
-                </div>
-                <div>
-                    if (!inventory || inventory.length === 0) return <p>No data available.</p>;
                 </div>
             </div>
         </>
