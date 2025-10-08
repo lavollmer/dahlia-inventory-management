ecfa7787fcc946b21fd02a04c41e82d2d55fff8d
Laura <laura.e.vollmer@gmail.com>
2025-10-08 11:59:29 -0500

    Updating X  Axis on purchase trends line

--- src/graphs/PurchaseTrends.jsx
+++ src/graphs/PurchaseTrends.jsx
@@ -1,20 +1,29 @@
 import "../App.css"
-import {LineChart, XAxis, YAxis, Tooltip, Line} from 'recharts';
+import { LineChart, XAxis, YAxis, Tooltip, Line } from 'recharts';
 
-const PurchaseTrends = ({inventory}) => {
-  return (
-    <div>
-        <h1>Purchase Trends</h1>
+
+const PurchaseTrends = ({ inventory }) => {
+    if (!inventory || inventory.length === 0) {
+        return <p>No data available.</p>;
+    }
+
+    const formattedData = inventory.map(item => ({
+        ...item,
+        purchase_year: new Date(item.purchase_year).getFullYear()
+    }));
+    return (
         <div>
-            <LineChart data={inventory}>
-                <YAxis />
-                <XAxis />
-                <Tooltip />
-                <Line type="monotone" dataKey="number_of_tubers" stroke="#8884d8"/>
-            </LineChart>
+            <h1>Purchase Trends</h1>
+            <div>
+                <LineChart width={600} height={300} data={formattedData}>
+                    <YAxis strokeDasharray="3 3" />
+                    <XAxis dataKey="purchase_year" />
+                    <Tooltip />
+                    <Line type="monotone" dataKey="number_of_tubers" stroke="#8884d8" />
+                </LineChart>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default PurchaseTrends
