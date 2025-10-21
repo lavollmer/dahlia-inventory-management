45581bf8d4ac5a23ffadda4fe41c6a463d3f87fa
Laura <laura.e.vollmer@gmail.com>
2025-10-21 16:34:25 -0500

    Debugging backend CORS issues

--- server/index.js
+++ server/index.js
@@ -10,12 +10,14 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-  origin: "https://dahlia-petal-ledger.netlify.app", 
-  methods: ["GET", "POST", "PUT", "DELETE"]
-}));
+app.use(cors());
 
-// app.use(cors());
+
+// After your cors middleware:
+app.options('*', (req, res) => {
+  res.sendStatus(204);
+});
+
 
 // Middleware
 app.use(express.json());
@@ -30,7 +32,7 @@ mongoose.connect(process.env.MONGO_URI)
     .catch(err => console.error('Mongoose Error:', err));
 
 // GET endpoint
-app.get('/', (req, res) => {
+app.get('/inventory', (req, res) => {
     res.send('Hello!')
     res.json({ message: 'Inventory list here' })
 });
