79822708681292a4dbe77170ee1acae372577218
Laura <laura.e.vollmer@gmail.com>
2025-10-21 16:50:07 -0500

    Update the route

--- server/index.js
+++ server/index.js
@@ -10,7 +10,9 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+   origin: 'https://dahlia-petal-ledger.netlify.app'
+}));
 
 // Middleware
 app.use(express.json());
@@ -25,7 +27,7 @@ mongoose.connect(process.env.MONGO_URI)
     .catch(err => console.error('Mongoose Error:', err));
 
 // GET endpoint
-app.get('/inventory', (req, res) => {
+app.get('/', (req, res) => {
     res.json({ message: 'Inventory list here' })
 });
 
